93b4c6f2a6fe4e94e2f3e567ac5b930c19707614
Halima Akther <halimaakthermou004@gmail.com>
2026-04-17 15:48:27 +0600

    implement homepage with Banner and FriendsList components

--- keenkeeper/src/app/page.js
+++ keenkeeper/src/app/page.js
@@ -1,6 +1,5 @@
 import Banner from "@/components/Banner";
 import FriendsList from "@/components/FriendsList";
-
 export default function Home() {
   return (
     <div className="container mx-auto px-4 py-10">
